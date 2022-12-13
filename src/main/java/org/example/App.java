e3e9cbdec173ef24d69d2fc42d272718bab35190
DIN-GS <nikita.ivakin@nure.ua>
2022-12-14 00:19:06 +0200

    wrote code, but how should i test it?

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.io.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,27 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Human Vasya = new Human("Вася", 22, "чоловік");
+        Human Maruna = new Human("Марина",23,"жінка");
+
+        File output = new File("src/main/java/org/example/output");
+        output.mkdir();
+        File result = new File("src/main/java/org/example/output/result.csv");
+        try{
+            result.createNewFile();
+            FileWriter fileWriter = new FileWriter(result.getAbsolutePath());
+            fileWriter.write(Vasya+"\n");
+            fileWriter.write(Maruna+"\n");
+            fileWriter.close();
+            System.out.println("Wrote to the result.csv");
+            BufferedReader bufferedReader =new BufferedReader(new FileReader(result.getName()));
+            System.out.println(bufferedReader.readLine());
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println("Oooops");
+        }
+
+
+
     }
 }
